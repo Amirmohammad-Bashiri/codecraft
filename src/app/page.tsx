6d740ddc1ad192c0f550ccba8b4a8dadcb464bb2
Amirmohammad Bashiri <amirmohamad.bashiri@gmail.com>
2023-12-24 10:00:12 +0330

    Auth modal setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import AuthModal from "@/components/modals/auth-modal";
 import Navbar from "@/components/navbar";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
+      <AuthModal />
     </main>
   );
 }
